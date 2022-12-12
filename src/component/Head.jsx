bb238f5e3be94d0cf51f7760098b0d3e81d2ac78
chezapangestu <cheza426@gmail.com>
2022-12-12 16:56:55 +0700

    add mb in org experience

--- src/component/Head.jsx
+++ src/component/Head.jsx
@@ -139,7 +139,7 @@ function Head(){
                                 <li className="font-normal text-gray-500 dark:text-gray-400">Responsible for the communication, publication and documentation works of BUILD A BOT</li>  
                             </div>
                         </li>
-                        <li className="ml-6">
+                        <li className="mb-10 ml-6">
                             <span className="flex absolute -left-3 justify-center items-center w-6 h-6 bg-blue-200 rounded-full ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
                                 <svg aria-hidden="true" className="w-3 h-3 text-blue-600 dark:text-blue-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
                             </span>
