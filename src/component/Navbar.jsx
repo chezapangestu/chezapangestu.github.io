f03f7fea0f2679a153ad2d68a07f0b9ad355a702
chezapangestu <cheza426@gmail.com>
2022-12-20 14:32:27 +0700

    fix the navbar and some heading additional changes

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -30,33 +30,28 @@ function Navbar(){
       <div className="container flex flex-wrap items-center justify-between mx-auto">
           <div className="flex items-center">
           <button onClick={handleClickScroll} value="head" className="self-center tracking-[-.10em] max-[360px]:text-[0px] text-xl sm:text-xl md:text-2xl lg:text-2xl font-bold whitespace-nowrap dark:text-white py-3 pl-5 mb-1">vijaycheza.</button>
-              {/* <img src="https://flowbite.com/docs/images/logo.svg" className="h-6 mr-3 sm:h-9" alt="Flowbite Logo" /> */}
-              {/* <span className="self-center text-2xl font-bold whitespace-nowrap dark:text-white  pl-5 mb-2">cheza's</span> */}
           </div>
 
           <ul className="flex flex-row font-medium text-xs text-right mt-4 border border-gray-100 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 dark:border-gray-500 mb-4 sm:mb-4 md:mb-0 lg:mb=0">
               <li>
-              <button onClick={handleClickScroll} value="body-experience" className="py-2 px-2 rounded text-gray-700 dark:text-white hover:text-blue-300 dark:hover:text-blue-300 ">Experience</button>
+              <button onClick={handleClickScroll} value="body-experience" className="py-2 px-2 rounded text-gray-700 dark:text-white hover:text-blue-300 dark:hover:text-blue-300 focus:text-blue-300">Experience</button>
               </li>
               <li>
-              <button onClick={handleClickScroll} value="portfolio" className="py-2 px-2 rounded text-gray-700 dark:text-white hover:text-blue-300 dark:hover:text-blue-300 ">Projects</button>
+              <button onClick={handleClickScroll} value="portfolio" className="py-2 px-2 rounded text-gray-700 dark:text-white hover:text-blue-300 dark:hover:text-blue-300 focus:text-blue-300">Projects</button>
               </li>
               <li>
-              <button onClick={handleClickScroll} value="contact-me" className="py-2 px-2 rounded text-gray-700 dark:text-white hover:text-blue-300 dark:hover:text-blue-300 ">Contact</button>
+              <button onClick={handleClickScroll} value="contact-me" className="py-2 px-2 rounded text-gray-700 dark:text-white hover:text-blue-300 dark:hover:text-blue-300 focus:text-blue-300">Contact</button>
               </li>
 
               <li>
-
-                  <label class="inline-flex relative items-center cursor-pointer mt-1.5 ml-2 lg:mt-2 mr-2">
+                  <label class="rounded-xl inline-flex relative items-center cursor-pointer mt-1.5 ml-2 lg:mt-2 mr-2">
                     <input type="checkbox" value="" className="sr-only peer" />
                     <div onClick={handleThemeSwitch} className="w-9 h-5 bg-gray-200 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-gray-500 after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white dark:after:bg-white-500 after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all dark:border-gray-600 peer-checked:bg-dark-black"></div>
-                   
-                    </label>
-
+                  </label>
               </li>
 
           </ul>
-          {/* </div> */}
+
       </div>
       </nav>
 
